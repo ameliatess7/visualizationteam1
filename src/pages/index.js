6080a09a51ffdaf9a0595b0da058c5b8351d2751
ameliatess7 <49301875+ameliatess7@users.noreply.github.com>
2019-09-23 01:03:22 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,6 +34,19 @@ class RootIndex extends React.Component {
     )
   }
 }
+        function initViz() {
+            var containerDiv = document.getElementById("vizContainer"),
+                url = "https://public.tableau.com/views/OlympicGoldMetalsByCountry/Sheet1?:embed=y&:display_count=yes&publish=yes&:origin=viz_share_link",
+                options = {
+                    hideTabs: true,
+                    onFirstInteractive: function () {
+                        console.log("Run this code when the viz has finished loading.");
+                    }
+                };
+
+            var viz = new tableau.Viz(containerDiv, url, options);
+            // Create a viz object and embed it in the container div.
+        }
 
 
 export default RootIndex
